8b49c2d70044b9879954bc33953c56fc2fb84361
Cindy Jean <cindy.jean83@gmail.com>
2022-05-26 20:45:24 +0200

    fix: test without data

--- src/lib/table.jsx
+++ src/lib/table.jsx
@@ -2,26 +2,26 @@ import React, { useEffect, useState } from "react";
 import "./table.css";
 import employees from "../employees.json";
 
-const headCells = [
-  { label: "First Name", data: "firstName" },
-  { label: "Last Name", data: "lastName" },
-  { label: "Start Date", data: "startDate" },
-  { label: "Department", data: "department" },
-  { label: "Date of Birth", data: "dateOfBirth" },
-  { label: "Street", data: "street" },
-  { label: "City", data: "city" },
-  { label: "State", data: "state" },
-  { label: "Zip Code", data: "zipCode" },
-];
 
 export function Table() {
-  const [data, setData] = useState(null);
+  const headCells = [
+    { label: "First Name", data: "firstName" },
+    { label: "Last Name", data: "lastName" },
+    { label: "Start Date", data: "startDate" },
+    { label: "Department", data: "department" },
+    { label: "Date of Birth", data: "dateOfBirth" },
+    { label: "Street", data: "street" },
+    { label: "City", data: "city" },
+    { label: "State", data: "state" },
+    { label: "Zip Code", data: "zipCode" },
+  ];
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    if (data === null) {
-      setData(employees);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (data === null) {
+  //     setData(employees);
+  //   }
+  // }, []);
 
   return (
     <table>
@@ -33,13 +33,13 @@ export function Table() {
         </tr>
       </thead>
       <tbody>
-        {employees.map((employee, employeeIndex) => (
+        {/* {employees.map((employee, employeeIndex) => (
           <tr key={employeeIndex}>
             {Object.keys(employee).map((cell, cellIndex) => (
               <td key={cellIndex}>{employee[cell]}</td>
             ))}
           </tr>
-        ))}
+        ))} */}
       </tbody>
     </table>
   );
